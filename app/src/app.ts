d194dcb7d8a8513f747e8474b70a283c553984e5
Marcin Skowyra <m.skowyra@clearcode.cc>
2019-05-27 19:09:56 +0200

    Post reute

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,22 +1,20 @@
 import "reflect-metadata";
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as cors from "cors";
 import { createConnection } from "typeorm";
+var app = express();
 
 createConnection().then(async connection => {
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+  app.use(cors())
+  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(bodyParser.json())
 
-var routes = require('./routes'); //importing route
-routes(app)
+  const routes = require('./routes/routes'); //importing route
+  routes(app)
 
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`server on port ${PORT}`)
-});
+  const PORT = 5000;
+  app.listen(PORT, () => {
+    console.log(`server on port ${PORT}`)
+  });
 }).catch(error => console.log(error));
